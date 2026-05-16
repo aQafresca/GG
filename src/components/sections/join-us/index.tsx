8d96bbeccbc0a7c5550f4fcf78d01999a43751fe
Siarhei <innxfect@gmail.com>
2026-05-16 08:26:47 +0300

    fix(netlify): cancel last update

--- src/components/sections/join-us/index.tsx
+++ src/components/sections/join-us/index.tsx
@@ -4,24 +4,22 @@ import styles from './index.module.scss';
 
 export const JoinUsSections = () => {
   return (
-    <section className={styles.join}>
-      <div className={`container ${styles.wrapper}`}>
-        <div className={styles.inner}>
-          <div>
-            <p className={styles.title}>Ready to have expiry</p>
-            <p className={styles.subtitle}>The event of lifetime</p>
-          </div>
-          <p className={styles.description}>
-            When it comes to event production in Miami, Florida, Event Factor delivers experiences that speak for
-            themselves. Book your spot now and secure memories that will last forever!
-          </p>
+    <section className={`container ${styles.join}`}>
+      <div className={styles.inner}>
+        <div>
+          <p className={styles.title}>Ready to have expiry</p>
+          <p className={styles.subtitle}>The event of lifetime</p>
         </div>
-        <div className={styles.inner}>
-          <p>Давайте сделаем это</p>
-          <Button size={'lg'} variant={'accent'}>
-            8 (029) 180 14 38
-          </Button>
-        </div>
+        <p className={styles.description}>
+          When it comes to event production in Miami, Florida, Event Factor delivers experiences that speak for
+          themselves. Book your spot now and secure memories that will last forever!
+        </p>
+      </div>
+      <div className={styles.inner}>
+        <p>Давайте сделаем это</p>
+        <Button size={'lg'} variant={'accent'}>
+          8 (029) 180 14 38
+        </Button>
       </div>
     </section>
   );
