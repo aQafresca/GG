54f9e17cda260bb03fe3f770303b25fdbcddfafa
Siarhei <innxfect@gmail.com>
2026-05-16 07:56:57 +0300

    refactor(sections): update imports

--- src/components/sections/hero/index.tsx
+++ src/components/sections/hero/index.tsx
@@ -1,3 +1,5 @@
+import dots from 'src/assets/images/svg/dots.svg';
+
 import { renderTitle } from '@/shared/helpers/text-decoration';
 
 import styles from './index.module.scss';
@@ -11,7 +13,7 @@ export const HeroSection = ({ title, slogan }: IProps) => {
   return (
     <section className={`container ${styles.hero}`}>
       <div className={styles.inner}>
-        <img src="src/assets/images/svg/dots.svg" width={70} height={40} alt="dots decoration" />
+        <img src={dots} width={70} height={40} alt="dots decoration" />
         <p className={styles.slogan}>{slogan}</p>
         <h1 className={styles.title}>{renderTitle(title)}</h1>
       </div>
